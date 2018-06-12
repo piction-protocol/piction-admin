aa6a226288c80f2613e41b193bef209eabb3532b
ki <sk.kwon@battleent.com>
2018-06-12 17:47:44 +0900

    Add bootstrap

---
+++ src/components/Wallet.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Wallet',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App2'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
+import Wallet from '@/components/Wallet'
 
 Vue.use(Router)
 
@@ -8,8 +9,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
+    }, {
+      path: '/wallet',
+      name: 'Wallet',
+      component: Wallet
     }
   ]
 })
