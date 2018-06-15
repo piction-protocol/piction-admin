ae2b49c6e1faa5cd6a8fd744e9a83328f79297cf
sk.kwon <sk.kwon@battleent.com>
2018-06-15 15:17:15 +0900

    main.js provider 설정 제거

--- src/main.js
+++ src/main.js
@@ -8,19 +8,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
-
 Vue.use(BootstrapVue);
-
-if (web3 && web3.currentProvider) {
-  web3 = new Web3(web3.currentProvider)
-} else {
-  web3 = new Web3(new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws'))
-}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: {App},
-  template: '<App/>'
+  template: '<App/>',
 })
