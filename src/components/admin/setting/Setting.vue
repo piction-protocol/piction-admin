26bc549d3f256a367d7ff0cf4d28279aff528ae4
ki <sk.kwon@battleent.com>
2018-06-19 16:14:16 +0900

    컨트렉트 주소 설정 페이지 구현

---
+++ src/components/admin/setting/Setting.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <SetAddress v-for="address in addressList()"
+                class="component"
+                :key="address.storageKey"
+                :title="address.title"
+                :storageKey="address.storageKey"/>
+  </div>
+</template>
+
+<script>
+  import SetAddress from './SetAddress'
+
+  export default {
+    name: 'Setting',
+    components: {SetAddress},
+    data() {
+      return {
+        addressList: function () {
+          return [
+            {title: 'PXL Contract address', storageKey: this.localStorageKey.PXLAddress},
+            {title: 'Whitelist Contract address', storageKey: this.localStorageKey.whitelistAddress},
+            {title: 'Sale Contract address', storageKey: this.localStorageKey.saleAddress},
+          ]
+        }
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .component {
+    margin-bottom: 10px;
+  }
+</style>
