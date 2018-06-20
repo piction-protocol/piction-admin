4e5ab6cbd70af10c05da278522ed177d9a3d6449
ki <sk.kwon@battleent.com>
2018-06-20 15:03:30 +0900

    ProgressModal 컴포넌트 구현

---
+++ src/ProgressModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-modal
+      v-model="modalShow"
+      :centered="true"
+      :no-close-on-backdrop="true"
+      :hide-header="true"
+      :hide-footer="true">
+      <h5>잠시 기다려주세요!</h5>
+      <b-progress :value="100" variant="danger" :animated="true"></b-progress>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProgressModal',
+    data() {
+      return {
+        modalShow: false
+      }
+    },
+    created() {
+      this.$EventBus.$on('showProgressModal', () => this.modalShow = true);
+      this.$EventBus.$on('hideProgressModal', () => this.modalShow = false);
+    }
+  }
+</script>
+
+<style>
+</style>
