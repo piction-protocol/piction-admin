a3684f3fd402072a820a0bff1a64cd6f5e4e92fb
ki <sk.kwon@battleent.com>
2018-06-21 14:35:00 +0900

    wallet 페이지 제거

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Wallet from '@/components/Wallet'
 import PXL from '@/components/admin/pxl/PXL'
 import Whitelist from '@/components/admin/whitelist/Whitelist'
 import Sale from '@/components/admin/sale/Sale'
@@ -27,11 +26,6 @@ const require = async (to, from, next) => {
 const router = new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'Wallet',
-      component: Wallet
-    },
     {
       path: '/admin',
       redirect: '/admin/pxl',
