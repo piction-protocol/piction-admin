dd45e60a9d7cb52d051bfdb715bf71c171bbf9ac
sk.kwon <sk.kwon@battleent.com>
2018-06-22 14:43:17 +0900

    provider 컴포넌트 작성

---
+++ src/components/Provider.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-alert show dismissible fade variant="secondary" class="provider">
+      <span class="network">[{{network}}]</span>
+      <a target="_blank" class="alert-link"
+         v-bind:href="getEtherscanURLForAddress(account)">{{account}}</a>
+    </b-alert>
+  </div>
+</template>
+
+<script>
+  import Web3 from 'web3'
+
+  export default {
+    name: 'Provider',
+    data() {
+      return {
+        account: null,
+      }
+    },
+    created() {
+      web3 = new Web3(web3.currentProvider);
+      web3.eth.getAccounts((err, _account) => {
+        this.account = _account[0]
+        web3.currentProvider.publicConfigStore.on('update', (provider) => {
+          if (this.account.toLowerCase() != provider.selectedAddress.toLowerCase()) {
+            window.location.reload()
+          }
+        });
+      });
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .provider {
+    margin: 10px;
+  }
+
+  .network {
+    color: red;
+    font-weight: bold;
+  }
+</style>
