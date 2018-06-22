3a5e06621da3096e741ab67c3eab31808e5c2eb7
jhSeo <jh.seo@battleent.com>
2018-06-22 18:36:15 +0900

    TokenDistributor 상장시간을 입력받도록 수정

--- src/components/tokenDistributor/SetCriterionTime.vue
+++ src/components/tokenDistributor/SetCriterionTime.vue
@@ -6,10 +6,18 @@
       <p class="card-text">
         상장시간을 현재시간으로 설정합니다.
       </p>
-      <p>현재 설정된 상장시간: {{ criterionTime == 0 ? "없음" : criterionTimeToKST }}</p>
-      <b-btn variant="info"
-             v-on:click="setCriterionTime">실행
-      </b-btn>
+      <p>현재 설정된 상장시간: {{ registedCriterionTime == 0 ? "없음" : criterionTimeToKST }}</p>
+      <b-input-group>
+        <b-form-input id="criterionDateTime"
+                      v-model.trim="criterionDateTime"
+                      type="datetime-local"
+                      :state="criterionDateTimeState"></b-form-input>
+        <b-input-group-append>
+          <b-btn variant="info"
+                 :disabled="!criterionDateTimeState"
+                 v-on:click="setCriterionTime">실행
+          </b-btn>
+        </b-input-group-append>
       </b-input-group>
       <div v-if="transactionHash">
         TransactionHash : <a target="_blank"
@@ -23,26 +31,30 @@
   export default {
     name: 'DistributorSetCriterionTime',
     computed: {
+      criterionDateTimeState() {
+        return this.criterionDateTime && this.criterionDateTime.length > 0 ? true : false;
+      },
       criterionTimeToKST() {
-        return new Date(Number(this.criterionTime)).toLocaleString();
+        return new Date(Number(this.registedCriterionTime)).toLocaleString();
       }
     },
     props: ['contract'],
     data() {
       return {
-        criterionTime: 0,
+        criterionDateTime: null,
+        registedCriterionTime: 0,
         transactionHash: null,
       }
     },
     methods: {
       getCriterionTime() {
         this.contract.methods.criterionTime().call((err, receipt) => {
-          this.criterionTime = receipt
+          this.registedCriterionTime = receipt
         })
       },
       setCriterionTime() {
+        var timestamp = Date.parse(criterionDateTime.value)
         this.$EventBus.$emit('showProgressModal');
-        var timestamp = new Date().getTime();
         this.contract.methods.setCriterionTime(timestamp).send()
         .on('transactionHash', (hash) => {
           this.$EventBus.$emit('SetMessageProgressModal', hash);
