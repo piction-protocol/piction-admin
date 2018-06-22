32592bb87139c210dc7e330fda5a194699590d91
sk.kwon <sk.kwon@battleent.com>
2018-06-22 14:56:12 +0900

    Modal에 transactionHash 링크를 표시하도록 수정

--- src/components/ProgressModal.vue
+++ src/components/ProgressModal.vue
@@ -8,6 +8,9 @@
       :hide-footer="true">
       <h5>잠시 기다려주세요!</h5>
       <b-progress :value="100" variant="danger" :animated="true"></b-progress>
+      <div v-if="transactionHash" class="transaction">
+        <a target="_blank" v-bind:href="getEtherscanURL(transactionHash)">{{transactionHash}}</a>
+      </div>
     </b-modal>
   </div>
 </template>
@@ -17,15 +20,27 @@
     name: 'ProgressModal',
     data() {
       return {
-        modalShow: false
+        modalShow: false,
+        transactionHash: null
       }
     },
     created() {
-      this.$EventBus.$on('showProgressModal', () => this.modalShow = true);
+      this.$EventBus.$on('showProgressModal', () => {
+        this.transactionHash = null;
+        this.modalShow = true;
+      });
       this.$EventBus.$on('hideProgressModal', () => this.modalShow = false);
+      this.$EventBus.$on('SetMessageProgressModal', (hash) => {
+        this.transactionHash = hash;
+      });
     }
   }
 </script>
 
 <style>
+  .transaction {
+    display: block;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
 </style>
